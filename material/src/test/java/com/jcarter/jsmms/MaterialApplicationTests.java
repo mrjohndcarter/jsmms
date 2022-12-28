a8009be9fbd4c5ac317069614b1975e352febaa2
John Carter <mrjohndcarter@gmail.com>
2022-12-28 14:56:18 -0500

    bringing in stuff from springboot initalizer

---
+++ material/src/test/java/com/jcarter/jsmms/MaterialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcarter.jsmms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
